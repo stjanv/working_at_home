e212b3dd17104926380dc8e53c0d764651ba505f
stjanv <50593689+stjanv@users.noreply.github.com>
2019-06-03 21:23:20 +0300

    Update bank_deposit.py

--- bank_deposit.py
+++ bank_deposit.py
@@ -1,9 +1,38 @@
-def deposit(count_of_money, years):
-    i = 1
-    while i <= years:
-        count_of_money = count_of_money+(count_of_money*0.1)
-        i += 1
-    return count_of_money
+from datetime import datetime
+import functools
+from functools import singledispatch
+from functools import lru_cache
+import numbers
 
 
-print(deposit(1000, 2))
+def lfu_cache(func):
+    cache={}
+    def wrapper(n):
+        nonlocal cache
+        if n in cache:
+            cache[n] = cache[n] + 1
+        else:
+            cache = {n: 1}
+        func(n)
+
+
+        return cache
+    return wrapper
+
+
+
+@lfu_cache
+def func(n):
+    n=int(n)
+    n+=1
+    return n
+
+func(2)
+func(3)
+func(4)
+func(2)
+func(6)
+func(6)
+print(func(4))
+
+
