c9bccaed352814cb04e451dc8d32f78856cf6689
stjanv <ruslanstojnov@gmail.com>
2019-06-07 18:15:31 +0300

    update

--- checking.py
+++ checking.py
@@ -1,131 +1,94 @@
-lab = """
-111111111111111111
-1x0010000000000001
-111011101111111101
-10100000000000000#
-101011010101111111
-101000010100001001
-100011110101111101
-101110010100000001
-100000000101101111
-101101111100100001
-101001000000101001
-101101111111101001
-101000000000001001
-101011110100101001
-101000010111111001
-111011110100000001
-101000000101111111
-101011110101010101
-100000010100000001
-111111111111111111""".split()
-labirint2 = []
-tern = []
-was = {}
-neighbour_x = None
-neighbour_y = None
-current_x = None
-current_y = None
+class CacheNode(object):
+    def __init__(self, key, value, freq_node, pre, nxt):
+        self.key = key
+        self.value = value
+        self.freq_node = freq_node
+        self.pre = pre  # prev CacheNode
+        self.nxt = nxt  # next CacheNode
 
-# превращение строки в список строк
-def Transmut_Str_into_Mass(string):
-    for i in lab:
-        labirint2.append(list(i))
-    for i in range(len(labirint2)):
-        for j in range(len(labirint2[i])):
-            if labirint2[i][j] == '1':
-                labirint2[i][j] = '-1'
+    def free_myself(self):
+        if self.freq_node.cache_head == self.freq_node.cache_tail:
+            self.freq_node.cache_head = self.freq_node.cache_tail = None
+        elif self.freq_node.cache_head == self:
+            self.nxt.pre = None
+            self.freq_node.cache_head = self.nxt
+        elif self.freq_node.cache_tail == self:
+            self.pre.nxt = None
+            self.freq_node.cache_tail = self.pre
+        else:
+            self.pre.nxt = self.nxt
+            self.nxt.pre = self.pre
 
-#coder x and #
-def coder():
-    labirint2[point_out[0]][point_out[1]] = 0
-    labirint2[point_in[0]][point_in[1]]=1
-    return labirint2
-
-# печать списка в виде матрицы
-def PrintLabirint(labirint):
-    for i in labirint:
-        for j in i:
-            print("{:^3}".format(j), end=",")
-        print("")
+        self.pre = None
+        self.nxt = None
+        self.freq_node = None
 
 
-# поиск начальной точки
-def Found_point_in(labirint):
-    for i in range(len(labirint2)):
-        for j in range(len(labirint2[i])):
-            if labirint[j][i] == 'x':
-                was[('start_cord_x', 'start_cord_y')] = (j, i)
-                return (j, i)
+class FreqNode(object):
+    def __init__(self, freq, pre, nxt):
+        self.freq = freq
+        self.pre = pre  # previous FreqNode
+        self.nxt = nxt  # next FreqNode
+        self.cache_head = None  # CacheNode head under this FreqNode
+        self.cache_tail = None  # CacheNode tail under this FreqNode
 
+    def count_caches(self):
+        if self.cache_head is None and self.cache_tail is None:
+            return 0
+        elif self.cache_head == self.cache_tail:
+            return 1
+        else:
+            return '2+'
 
-# search exit point
-def Found_point_out(labirint):
-    for i in range(len(labirint2)):
-        for j in range(len(labirint2[i])):
-            if labirint[i][j] == '#':
-                was[('exit_cord_x', 'exit_cord_y')] = (j, i)
-                return (j, i)
+    def remove(self):
+        if self.pre is not None:
+            self.pre.nxt = self.nxt
+        if self.nxt is not None:
+            self.nxt.pre = self.pre
 
+        pre = self.pre
+        nxt = self.nxt
+        self.pre = self.nxt = self.cache_head = self.cache_tail = None
 
-# функция поиска выхода
-def Found(labirint, pointOut):
-    way = 1
-    for i in range(len(labirint) * len(labirint[0])):
-        way += 1
-        for y in range(len(labirint)):
-            for x in range(len(labirint[y])):
-                if labirint[y][x] == (way - 1):
-                    if y > 0 and labirint[y - 1][x] == '0':
-                        labirint[y - 1][x] = way
-                    if y < (len(labirint) - 1) and labirint[y + 1][x] == '0':
-                        labirint[y + 1][x] = way
-                    if x > 0 and labirint[y][x - 1] == '0':
-                        labirint[y][x - 1] = way
-                    if x < (len(labirint[y]) - 1) and labirint[y][x + 1] == '0':
-                        labirint[y][x + 1] = way
+        return (pre, nxt)
 
-                    if (abs(y - pointOut[0]) + abs(x - pointOut[1])) == 1:
-                        labirint[pointOut[0]][pointOut[1]] = way
-                        return True
-    return False
+    def pop_head_cache(self):
+        if self.cache_head is None and self.cache_tail is None:
+            return None
+        elif self.cache_head == self.cache_tail:
+            cache_head = self.cache_head
+            self.cache_head = self.cache_tail = None
+            return cache_head
+        else:
+            cache_head = self.cache_head
+            self.cache_head.nxt.pre = None
+            self.cache_head = self.cache_head.nxt
+            return cache_head
 
-#way print
-def printWay(labirint, pointOut):
-    y = pointOut[0]
-    x = pointOut[1]
-    way = labirint[y][x]
-    result = list(range(way))
-    while (way):
-        way -= 1
-        if y > 0 and labirint[y - 1][x] == way:
-            y -= 1
-            result[way] = 'down'
-        elif y < (len(labirint) - 1) and labirint[y + 1][x] == way:
-            result[way] = 'up'
-            y += 1
-        elif x > 0 and labirint[y][x - 1] == way:
-            result[way] = 'right'
-            x -= 1
-        elif x < (len(labirint[y]) - 1) and labirint[y][x + 1] == way:
-            result[way] = 'left'
-            x += 1
+    def append_cache_to_tail(self, cache_node):
+        cache_node.freq_node = self
 
-    return result[1:]
+        if self.cache_head is None and self.cache_tail is None:
+            self.cache_head = self.cache_tail = cache_node
+        else:
+            cache_node.pre = self.cache_tail
+            cache_node.nxt = None
+            self.cache_tail.nxt = cache_node
+            self.cache_tail = cache_node
 
+    def insert_after_me(self, freq_node):
+        freq_node.pre = self
+        freq_node.nxt = self.nxt
 
+        if self.nxt is not None:
+            self.nxt.pre = freq_node
 
-Transmut_Str_into_Mass(lab)
-point_in = Found_point_in(labirint2)
-point_out = Found_point_out(labirint2)
-coder()
-PrintLabirint(labirint2)
-if not Found(labirint2, point_out):
-    print("Путь не найден!")
-else:
-    print('Exit is here')
-print()
-print(printWay(labirint2,point_out))
-print()
-PrintLabirint(labirint2)
-print(was)
+        self.nxt = freq_node
+
+    def insert_before_me(self, freq_node):
+        if self.pre is not None:
+            self.pre.nxt = freq_node
+
+        freq_node.pre = self.pre
+        freq_node.nxt = self
+        self.pre = freq_node
--- lesson7_task.py
+++ lesson7_task.py
@@ -1,96 +1,66 @@
-class CacheNode(object):
-    def __init__(self, key, value, freq_node, pre, nxt):
-        self.key = key
-        self.value = value
-        self.freq_node = freq_node
-        self.pre = pre  # prev CacheNode
-        self.nxt = nxt  # next CacheNode
+from line import line
+# class Person:
+#     def __init__(self, name, position, zp):
+#         self.name = name
+#         self.position = position
+#         self.zp = zp
+#
+#     def __str__(self):
+#         return 'Name: {},\nPosition: {},\nZp: {}'.format(self.name, self.position, self.zp)
+#
+#
+# class Manager(Person):
+#     def __init__(self, name, position, zp, proc):
+#         super().__init__(name, position, zp)
+#         self.proc = int(zp/proc)
+#
+#     def __str__(self):
+#         return 'Name: {},\nPosition: {},\nZp: {},\nBonus: {}'.format(self.name, self.position, self.zp, self.proc)
+#
+#
+# p = Person('Nick', 'Manager', 1500)
+# m = Manager('Vova','Admin',300,10)
+#
+#
+#
+# print(p)
+# print(line)
+# print(m)
 
-    def free_myself(self):
-        if self.freq_node.cache_head == self.freq_node.cache_tail:
-            self.freq_node.cache_head = self.freq_node.cache_tail = None
-        elif self.freq_node.cache_head == self:
-            self.nxt.pre = None
-            self.freq_node.cache_head = self.nxt
-        elif self.freq_node.cache_tail == self:
-            self.pre.nxt = None
-            self.freq_node.cache_tail = self.pre
+class ReadString:
+    string_for_check = ['{', '}', '[', ']', '(', ')', '<', '>']
+    string_for_check_right = ['{}', '[]', '()', '<>']
+    def __init__(self, string='String is empty'):
+        self.string = string
+
+    def __str__(self):
+        return 'Ur string is "{}"'.format(self.string)
+
+    def check_in_string(self):
+        boolean=True
+        for symbol in self.string:
+            if symbol not in ReadString.string_for_check:
+                boolean=False
+        if boolean:
+            return ('String contains only from this items list: '
+                    +str(ReadString.string_for_check)
+                    )
         else:
-            self.pre.nxt = self.nxt
-            self.nxt.pre = self.pre
+            return 'Ur string is bad'
 
-        self.pre = None
-        self.nxt = None
-        self.freq_node = None
-
-
-class FreqNode(object):
-    def __init__(self, freq, pre, nxt):
-        self.freq = freq
-        self.pre = pre  # previous FreqNode
-        self.nxt = nxt  # next FreqNode
-        self.cache_head = None  # CacheNode head under this FreqNode
-        self.cache_tail = None  # CacheNode tail under this FreqNode
-
-    def count_caches(self):
-        if self.cache_head is None and self.cache_tail is None:
-            return 0
-        elif self.cache_head == self.cache_tail:
-            return 1
-        else:
-            return '2+'
-
-    def remove(self):
-        if self.pre is not None:
-            self.pre.nxt = self.nxt
-        if self.nxt is not None:
-            self.nxt.pre = self.pre
-
-        pre = self.pre
-        nxt = self.nxt
-        self.pre = self.nxt = self.cache_head = self.cache_tail = None
-
-        return (pre, nxt)
-
-    def pop_head_cache(self):
-        if self.cache_head is None and self.cache_tail is None:
-            return None
-        elif self.cache_head == self.cache_tail:
-            cache_head = self.cache_head
-            self.cache_head = self.cache_tail = None
-            return cache_head
-        else:
-            cache_head = self.cache_head
-            self.cache_head.nxt.pre = None
-            self.cache_head = self.cache_head.nxt
-            return cache_head
-
-    def append_cache_to_tail(self, cache_node):
-        cache_node.freq_node = self
-
-        if self.cache_head is None and self.cache_tail is None:
-            self.cache_head = self.cache_tail = cache_node
-        else:
-            cache_node.pre = self.cache_tail
-            cache_node.nxt = None
-            self.cache_tail.nxt = cache_node
-            self.cache_tail = cache_node
-
-    def insert_after_me(self, freq_node):
-        freq_node.pre = self
-        freq_node.nxt = self.nxt
-
-        if self.nxt is not None:
-            self.nxt.pre = freq_node
-
-        self.nxt = freq_node
-
-    def insert_before_me(self, freq_node):
-        if self.pre is not None:
-            self.pre.nxt = freq_node
-
-        freq_node.pre = self.pre
-        freq_node.nxt = self
-        self.pre = freq_node
+    def chec_for_good_or_bad(self):
+        boolean = True
+        for i in range(len(self.string)-1,2):
+            if (self.string[i]+self.string[i+1]) in ReadString.string_for_check_right:
+                boolean = True
+            else:
+                boolean = False
+                break
+        return boolean
 
 
+
+s = ReadString(input())
+print(s)
+print(s.check_in_string())
+print(s.chec_for_good_or_bad())
